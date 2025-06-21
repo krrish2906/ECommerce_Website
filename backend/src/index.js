9bf81f4be66da95f9464acb0987f2ccacb7f5645
krrish2906 <krrishbansal290605@gmail.com>
2025-06-21 22:20:59 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://e-commerce-website-tau-six.vercel.app'],
     credentials: true
 }));
 app.use('/api', ApiRoutes);
